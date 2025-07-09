4f723b470920ce273ea0be46dd1ce74eab0e425d
kevinpineda02 <116850215+kevinpineda02@users.noreply.github.com>
2025-07-08 23:41:24 -0600

    Delete Emprendimiento-main directory

--- Emprendimiento-main/Resources/Js/login.js
+++
@@ -1,8 +0,0 @@
- function toggleModal() {
-    const modal = document.getElementById("infoModal");
-    if (modal.style.display === "flex") {
-      modal.style.display = "none";
-    } else {
-      modal.style.display = "flex";
-    }
-  }
